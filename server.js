ccbd90d1e694ee32361a20eb33031bac1193049e
raymondchiu <raymondchiu19@gmail.com>
2024-05-05 13:20:04 -0400

    added heroku port

--- server.js
+++ server.js
@@ -59,6 +59,6 @@ app.get('/api/:name', (request, response) => {
     }
 })
 
-app.listen(PORT, () => {
-    console.log(`The server is runinng on port ${PORT}!`)
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`The server is running on port ${PORT}!`)
 })
